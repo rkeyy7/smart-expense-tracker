9502e82838522d5e7eb97d421125682979f3d66f
rkeyy7 <rubencg1930@gmail.com>
2026-06-12 13:47:28 -0500

    implementacion autenticación JWT en frontend Actualizacion LoginPage para guardar el token en localStorage y limpiar el user_id.
    - Configuracion interceptor en axios.ts para enviar la cabecera Authorization: Bearer automáticamente.
    
    - Refactorizacion DashboardPage para proteger la ruta verificando el token.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
+# __init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+import os
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
-# Configuración (pon tu contraseña real aquí)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:rkeyy7@localhost:5432/smart_expense_db'
+# --- CONFIGURACIÓN BLINDADA DE CORS ---
+# 'headers' debe incluir explícitamente 'Authorization'
+CORS(app, resources={r"/*": {"origins": "*"}}, headers=['Content-Type', 'Authorization'], supports_credentials=True)
+
+app.config["JWT_SECRET_KEY"] = "rkeyy7"
+jwt = JWTManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:rkeyy7@localhost:5432/smart_expense_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Importamos las rutas al final para evitar circular imports
 from app import routes
--- app/models.py
+++ app/models.py
@@ -1,12 +1,33 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+from datetime import datetime
 
 class Transaccion(db.Model):
-    __tablename__ = 'transacciones'
-    
     id = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(10), nullable=False)
+    tipo = db.Column(db.String(20), nullable=False)
     monto = db.Column(db.Float, nullable=False)
     categoria = db.Column(db.String(50), nullable=False)
-    fecha = db.Column(db.DateTime, default=datetime.utcnow)
     descripcion = db.Column(db.String(200))
+    
+    # NUEVO: Campo de fecha (por defecto toma la fecha actual)
+    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    
+    # Relación: Un usuario tiene muchas transacciones
+    transacciones = db.relationship('Transaccion', backref='owner', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
