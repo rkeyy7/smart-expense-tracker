fb57e82a6c53c0ff8799128e575a4393d844d21b
rkeyy7 <rubencg1930@gmail.com>
2026-08-20 19:30:56 -0500

    fix: auto crear tablas en db

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
+from app import create_app, db
+
+app = create_app()
+
+# Esto crea las tablas si no existen antes de arrancar
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
